e4fc009e40a7f6b0c9ef64a9a3a9db7f274576e1
Tatiana <saharova-tanya12@yandex.ru>
2024-02-13 20:08:52 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = "https://056d3d37-f56b-4cfe-a641-5bf98ab64509.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+CREATE_USER_PATH = "/api/v1/users/"
+
+
